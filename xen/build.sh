59d65a33a5b7c7944ba8f11f3bfc2a95f78c23ce
James Bielman <jamesjb@galois.com>
2013-08-21 12:56:43 -0700

    Build system fixes.
    - Install mirari in build.sh if it isn't found in PATH.
    - Update .gitignore to ignore xl config file.
    - Change library name of xenstore-flask.

--- xen/build.sh
+++ xen/build.sh
@@ -1,4 +1,12 @@
-#! /bin/sh
+#! /bin/bash
+
+set -e
+
+# Install Mirari via opam if not installed.
+if ! hash mirari 2>/dev/null; then
+  echo "mirari not found (or not in PATH), installing via OPAM"
+  opam install -y mirari
+fi
 
 mirari configure --xen
 mirari build --xen
